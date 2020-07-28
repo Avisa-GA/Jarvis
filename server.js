9880a64fab9ed31335ae13775fb66a16d7b07515
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-07-28 12:20:16 -0500

    adds first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Jarvis is listening on port ${port}`)
+});
