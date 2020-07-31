5b8e8fc6fe83c748c74ffe9e90d0c4c81b39bd16
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-07-31 08:44:54 -0500

    fixes uncomented code issue

--- utils/am-message.js
+++ utils/am-message.js
@@ -1,21 +1,17 @@
 const time = require('./time');
 const customer = process.env.CUSTOMER;
 
-module.exports = async function(getWeather, getQuote) {
-        const {
-            temp,
-            weather
-        } = await getWeather();
-    
-        // const { 
-        //     quote, 
-        //     author } = await getQuote();
-            
-            let endsWithS = weather.description.substr(weather.description.length -1, 1) === 's';
+module.exports = async function (getWeather, getQuote) {
+    const {
+        temp,
+        weather
+    } = await getWeather();
 
-            let quote = 'I was a poet and didn\'t know it';
-            let author = 'unknown';
-    
-        return `Good Morning, ${customer}, the current temperature is ${temp} degrees Fahrenheit. \n\nAs of ${time()}, the current weather condition suggests ${endsWithS ? 'a ' + weather.description : weather.description}. \n\nHere is your Quote of the Day:\n\n"${quote}" -${author}`;
-         
+    const {
+        quote,
+        author
+    } = await getQuote();
+
+    return `Good Morning, ${customer}, the current temperature is ${temp} degrees Fahrenheit. \n\nAs of ${time()}, the current weather condition suggests ${weather.description}. \n\nHere is your Quote of the Day:\n\n"${quote}" -${author}`;
+
 }
