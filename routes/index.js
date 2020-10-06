98665f558b36f757e34cf5a10a21d16efc278e6c
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-10-06 14:20:22 -0500

    makes latest updates

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
     });
 });
 
+router.get('/github/curiosity-updates', function(req, res) {
+    console.log(req.body)
+    res.json({status: 'ok'})
+});
+
 module.exports = router;
